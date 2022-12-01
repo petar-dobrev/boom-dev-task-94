b843f575f363b77336707ada97109eec8e61e9cc
Petar Dobrev <petar.dobrev92@gmail.com>
2022-12-01 14:32:36 +0200

    feat: monkeys with bananas

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,11 +11,21 @@ export default class Application extends EventEmitter {
     super();
     this.emojis = [];
     this.banana = "🍌";
+    this.emojisContainer = document.querySelector("#emojis");
+
     this.emit(Application.events.READY);
   }
   setEmojis(emojis) {
     this.emojis = emojis;
+    const p = document.createElement("p");
+    p.innerText = this.emojis;
+    this.emojisContainer.innerHTML = "";
+    this.emojisContainer.appendChild(p);
   }
 
-  addBananas() {}
+  addBananas() {
+    const monkeys = this.emojis;
+    this.emojis = monkeys.map((monkey) => monkey + this.banana);
+    this.setEmojis(this.emojis);
+  }
 }
